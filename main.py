1660f1b1feb4517d60ddadd9f328ec7da6b94cd6
hujalex <ahuwindsor@gmail.com>
2025-07-05 11:46:37 -0400

    api server setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fuzzywuzzy import fuzz
+from enum import Enum
 from fastapi import FastAPI, app, HTTPException
 import pandas
 from pydantic import BaseModel
@@ -9,6 +10,11 @@ app = FastAPI()
 class user_list(BaseModel):
     user_list_info: dict
     target_name: str
+
+class ApiResponse(baseModel):
+    most_similar_name: str
+    status: str
+    score: float
 
 @app.post("/api")
 async def read_item(message : user_list):
@@ -23,8 +29,13 @@ async def read_item(message : user_list):
             fuzzy_ratios[i] = fuzz.ratio(user_names, target_name)
     
         max_ratio_idx = np.argmax(fuzzy_ratios)
+    
 
-        return user_list_info[user_names[max_ratio_idx]]
+        return ApiResponse(
+            most_similar_name = user_list_info[user_names[max_ratio_idx]],
+            status = "success",
+            score = fuzzy_ratios[max_ratio_idx]
+        )
         
     except Exception as e:
         raise HTTPException(status_code = 500, detail = f"Error in processing Post Request {e}")
